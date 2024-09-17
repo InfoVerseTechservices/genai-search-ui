e310b17ecd2d7b254b6eeea1752c2203f6a5361f
sravani586 <sravsreddy586@gmail.com>
2024-09-17 10:36:49 -0400

    commented upload option

--- components/EmptyChatMessageInput.tsx
+++ components/EmptyChatMessageInput.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import CopilotToggle from './MessageInputActions/Copilot';
 import Focus from './MessageInputActions/Focus';
-import uploadIcon from '../public/uploadIcon.svg'
+import uploadIcon from '../public/uploadIcon.svg';
 import Image from 'next/image';
 
 const EmptyChatMessageInput = ({
@@ -45,7 +45,8 @@ const EmptyChatMessageInput = ({
   const borderStyle = {
     border: '0.5px solid transparent',
     backgroundClip: 'padding-box',
-    background: 'linear-gradient(white, white) padding-box, linear-gradient(180deg, #FF0049 0%, #FFBE3B 25%, #00BB5C 50%, #187DC4 75%, #58268B 100%) border-box',
+    background:
+      'linear-gradient(white, white) padding-box, linear-gradient(180deg, #FF0049 0%, #FFBE3B 25%, #00BB5C 50%, #187DC4 75%, #58268B 100%) border-box',
     borderImageSlice: 1,
   };
 
@@ -64,21 +65,26 @@ const EmptyChatMessageInput = ({
         }
       }}
       className="w-full"
-    > <div className='flex flex-col gap-[2rem] items-center w-full text-center'>
-      <div style={borderStyle} className=" relative flex flex-col bg-white px-5 pt-5 pb-2 rounded-lg items-center w-[48rem] border">
-        <TextareaAutosize
-          ref={inputRef}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          minRows={6}
-          className="bg-transparent p-1 placeholder:text-[#ACACAC] text-sm self-start text-black resize-none focus:outline-none w-full max-h-24 lg:max-h-36 xl:max-h-48"
-          placeholder="Ask Coco..."
-        />
-        <Image src={uploadIcon} alt='colombo' className="absolute top-2 right-2 cursor-pointer" />
+    >
+      {' '}
+      <div className="flex flex-col gap-[2rem] items-center w-full text-center">
+        <div
+          style={borderStyle}
+          className=" relative flex flex-col bg-white px-5 pt-5 pb-2 rounded-lg items-center w-[48rem] border"
+        >
+          <TextareaAutosize
+            ref={inputRef}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            minRows={6}
+            className="bg-transparent p-1 placeholder:text-[#ACACAC] text-sm self-start text-black resize-none focus:outline-none w-full max-h-24 lg:max-h-36 xl:max-h-48"
+            placeholder="Ask Coco..."
+          />
+          {/* <Image src={uploadIcon} alt='colombo' className="absolute top-2 right-2 cursor-pointer" /> */}
 
-        {/* Co-pilot */}
+          {/* Co-pilot */}
 
-        {/* <div className="flex flex-row items-center justify-between mt-4">
+          {/* <div className="flex flex-row items-center justify-between mt-4">
           <div className="flex flex-row items-center space-x-1 -mx-2">
             <Focus focusMode={focusMode} setFocusMode={setFocusMode} />
           </div>
@@ -95,9 +101,15 @@ const EmptyChatMessageInput = ({
             </button>
           </div>
         </div> */}
-      </div>
-      <p className='text-[#ACACAC] text-sm w-[44rem]'>Welcome to GenAI Search, your go-to tool for instant answers and web exploration! <br/>
-      Simply type your question or topic of interest, and GenAI will provide you with accurate answers along with related links from the web. Whether you&apos;re seeking quick information or <br/> diving deeper into a topic, GenAI Search has you covered.</p>
+        </div>
+        <p className="text-[#ACACAC] text-sm w-[44rem]">
+          Welcome to GenAI Search, your go-to tool for instant answers and web
+          exploration! <br />
+          Simply type your question or topic of interest, and GenAI will provide
+          you with accurate answers along with related links from the web.
+          Whether you&apos;re seeking quick information or <br /> diving deeper
+          into a topic, GenAI Search has you covered.
+        </p>
       </div>
     </form>
   );

d4dea16f9b05f783aef695659e6acbe9766c2467
ForumShah8 <shahforu00@gmail.com>
2024-09-27 17:28:04 -0400

    reverting back the responsive ad changes

--- components/MessageBox.tsx
+++ components/MessageBox.tsx
@@ -1528,7 +1528,7 @@ const MessageBox = ({
             /> 
 }
 {callAd && 
-      <div className=" w-[300px] mt-10 flex-col items-center gap-2.5  h-[calc(100vh-110px)] hide-scrollbar overflow-y-auto overflow-x-hidden">
+      <div className=" w-[300px] mt-10 hidden lg:flex xl:flex flex-col items-center gap-2.5  h-[calc(100vh-110px)] hide-scrollbar overflow-y-auto overflow-x-hidden">
                <div className="w-[300px]  h-[250px] cursor-pointer">
                  <SideTopAdComponent divid={`top-message-${messageIndex-1}`} />
                </div>

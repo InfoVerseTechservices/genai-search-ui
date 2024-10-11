596cafb7f268e96e42a87a4c235f64e69faf36ed
colomboaiinfrasupport <166234819+colomboaiinfrasupport@users.noreply.github.com>
2024-10-11 08:05:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 const nextConfig = {
   // Use assetPrefix if it's a production environment
-  assetPrefix:
-    process.env.NODE_ENV === 'production'
-      ? process.env.NEXT_PUBLIC_SITE_URL_PROD
-      : undefined,
+  assetPrefix: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SITE_URL_PROD : undefined,
 
   // Enforce trailing slashes in URLs
   trailingSlash: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.googleusercontent.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
-  },
-};
+}
 
 module.exports = nextConfig;
+

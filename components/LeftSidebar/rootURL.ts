816b9b55d3ece1f1968400b0cdb20cbc444c2075
Ashwinkumar Pillai <ashwin@ashwinkumarsmbp.attlocal.net>
2024-10-11 16:28:02 -0400

    same site cookies set to lax

--- components/LeftSidebar/rootURL.ts
+++ components/LeftSidebar/rootURL.ts
@@ -3,7 +3,7 @@ import { OptionsType } from 'cookies-next/lib/types';
 export const COOKIE_OPTIONS: OptionsType = {
   path: '/',
   expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000 * 365),
-  sameSite: 'strict',
+  sameSite: 'lax',
   secure: true,
   domain: '.colomboai.com',
 };

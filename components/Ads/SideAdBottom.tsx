92ae905bcb598c5bb195bc6ac57e9d79fd157330
Zax-boop <rohan.arya01@gmail.com>
2024-12-12 15:03:48 -0600

    increased ad size

--- components/Ads/SideAdBottom.tsx
+++ components/Ads/SideAdBottom.tsx
@@ -66,7 +66,7 @@ const SideBottomAdComponent: React.FC<SideBottomAdComponentProps> = ({ divid }) 
       <Head>
         <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
       </Head>
-      <div id={divid} style={{ minWidth: '125px', minHeight: '100px' }}></div>
+      <div id={divid} style={{ minWidth: '400px', minHeight: '100px' }}></div>
     </>
   );
 };

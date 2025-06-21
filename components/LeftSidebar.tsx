830246ea891e3ff573b81d01d1fc46539be9c0ca
InfoVerse <infoversetech@gmail.com>
2025-06-21 09:14:57 +0530

    remove genAIImage and Video from LeftSideBar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -168,7 +168,7 @@ const LeftSidebar: FC = () => {
             label="New Chat"
           />
         </div>
-        <div className="flex flex-col items-center space-y-6 flex-grow">
+        {/* <div className="flex flex-col items-center space-y-6 flex-grow">
           <IconLink
             href="/image-generator"
             Icon={ImageGeneratorIcon}
@@ -181,7 +181,7 @@ const LeftSidebar: FC = () => {
             Icon={VideoGeneratorIcon}
             label="Video Gen"
           />
-        </div>
+        </div> */}
         <div className="flex flex-col items-center space-y-6 flex-grow">
           <IconLink
             href="https://colomboai.com/genai-search/library/"
